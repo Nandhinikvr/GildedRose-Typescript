d3920cd4e3f8900c764330d6822c53e94bcf2eb4
Nandhini <kvrnandhini@gmail.com>
2024-01-26 11:53:48 +0000

    glided rose

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -17,45 +17,57 @@ export class GildedRose {
         this.items = items;
     }
 
-    // delcare a function to update the quality property if each item in the array
+   // delcare a function to update the quality property if each item in the array
+
     updateQuality() {
-        // for each item in the array
         for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') 
+            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') // legendary item, never has to be sold or decreases in Quality
             {
-               this.items[i].sellIn = this.items[i].sellIn - 1;
-              if((this.items[i].quality > 0)) 
-              { 
-                if(this.items[i].name == 'Conjured Mana Cake')  
-                {                    
-                 this.items[i].quality = this.items[i].quality - 2  
-                }       
-                else if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') 
+                if (this.items[i].quality >= 0 && this.items[i].quality < 50)
                 {
-                    (this.items[i].sellIn > 0) ? (this.items[i].quality = this.items[i].quality - 1) : (this.items[i].quality = this.items[i].quality - 2)
-                } 
-                else if (this.items[i].name == 'Aged Brie') 
-                {
-                    (this.items[i].sellIn > 0) ? (this.items[i].quality = this.items[i].quality + 1) : (this.items[i].quality = this.items[i].quality + 2)
-                } else 
-                {
-                    if (this.items[i].sellIn <= 0)
+                    if (this.items[i].name == 'Aged Brie') 
                     {
-                        this.items[i].quality = 0;
+                        this.items[i].quality=  (this.items[i].sellIn > 0) ?  Math.min(this.items[i].quality + 1, 50): // increase by 1
+                        Math.min(this.items[i].quality + 2, 50); // increase by 2 if outdated
+                    } 
+                    else if (this.items[i].name == 'Conjured Mana Cake') 
+                    {
+                        this.items[i].quality=  (this.items[i].sellIn > 0) ?  Math.max(this.items[i].quality - 2, 0): // degrade by 2
+                        Math.max(this.items[i].quality - 4, 0); // degrade by 4 if outdated
+                    }
+                    else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert')
+                    {
+                        if (this.items[i].sellIn >= 11) 
+                        {
+                            this.items[i].quality = Math.min(this.items[i].quality + 1, 50); // increases by 1
+                        }
+                        else if (this.items[i].sellIn <= 10 && this.items[i].sellIn >= 6) 
+                        {
+                            this.items[i].quality = Math.min(this.items[i].quality + 2, 50); // increases by 2 when there are 10 days or less
+                        }
+                        else if (this.items[i].sellIn < 6 && this.items[i].sellIn > 0) 
+                        {
+                            this.items[i].quality = Math.min(this.items[i].quality + 3, 50); // increases by 3 when there are 5 days o less
+                        } 
+                        else
+                        {
+                            this.items[i].quality = 0; // drops to 0 after the concert
+                        }
                     }
                     else 
                     {
-                        this.items[i].quality = this.items[i].quality + 1;
-                        if (this.items[i].sellIn < 11)
-                            this.items[i].quality = this.items[i].quality + 1;
-                        if (this.items[i].sellIn < 6)
-                            this.items[i].quality = this.items[i].quality + 1
-                    }
-                }
-            }
-        }
+                        this.items[i].quality=  (this.items[i].sellIn > 0) ?  Math.max(this.items[i].quality - 1, 0): // degrade by 1
+                        Math.max(this.items[i].quality - 2, 0); // degrade by 2 if outdated
+                    } 
 
+                }               
+        
+                this.items[i].sellIn -= 1;
+            } 
         }
         return this.items;
     }
+
+
+
 }
--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -1,20 +1,272 @@
 import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
-describe('Gilded Rose', function () {
+// describe('Gilded Rose', function () {
 
-    it('Aged Brie Quality increases by 1 as its Sellin value approaches less than 6 ', function() {
+//     it('Aged Brie Quality increases by 1 as its Sellin value approaches when there are 5 days ', function() {
+//         const gildedRose = new GildedRose([ new Item('Aged Brie', 5, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(46);
+//     });
+    
+//     it('Aged Brie Quality increases by 1 as its Sellin value approaches when there are 10 days', function() {
+//         const gildedRose = new GildedRose([ new Item('Aged Brie', 10, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(46);
+//     });
+//     it('Aged Brie Quality increases by 1 as its Sellin value apporaches ', function() {
+//         const gildedRose = new GildedRose([ new Item('Aged Brie', 15, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(46);
+//     });
+
+//     it('Aged Brie Quality increases by 2 as its Sellin value less than 0 ', function() {
+//         const gildedRose = new GildedRose([ new Item('Aged Brie', -1, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(47);
+//     });
+//     it('Aged Brie Quality increases by 2 as its Sellin value equal to 0 ', function() {
+//         const gildedRose = new GildedRose([ new Item('Aged Brie', 0, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(47);
+//     });
+    
+//     it('Backstage passes  Quality increases by 1 as its SellIn value approaches', function() {
+//         const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 15, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(46);
+//     });
+
+//     it('Backstage passes Quality increases by 2 when there are 10 days', function() {
+//         const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 10, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(47);
+//     });
+//     it('Backstage passes Quality increases by 1 when there are 11 days', function() {
+//         const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 11, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(46);
+//     });
+
+//     it('Backstage passes Quality increases by 2 when there are 6 days', function() {
+//         const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 6, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(47);
+//     });
+
+//     it('Backstage passes Quality increases by 3 when there are 5 days', function() {
+//         const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 5, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(48);
+//     });
+
+//     it('Backstage passes Quality drops to 0 after the concert', function() {
+//         const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', -1, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(0);
+//     });
+//     it('Backstage passes Quality drops to 0 after the concert', function() {
+//         const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 0, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(0);
+//     });
+
+//     it('For any Product -> Foo other than Aged Brie,Sulphurus,BackStage Quality Decreases by 1 when there are 5 days ', function() {
+//         const gildedRose = new GildedRose([ new Item('Foo', 5, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(44);
+//     });
+
+//     it('For any Product -> Foo other than Aged Brie,Sulphurus,BackStage Quality Decreases by 1 when there are 10 days ', function() {
+//         const gildedRose = new GildedRose([ new Item('Foo', 10, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(44);
+//     });
+//     it('For any Product -> Foo other than Aged Brie,Sulphurus,BackStage Quality Decreases by 1 as its Sellin value approaches', function() {
+//         const gildedRose = new GildedRose([ new Item('Foo', 15, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(44);
+//     });
+
+//     it('For any Product -> Foo other than Aged Brie,Sulphurus,BackStage Quality Decreases by 2 when it passes the Sellin date ', function() {
+//         const gildedRose = new GildedRose([ new Item('Foo', -1, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(43);
+//     });
+
+//     it('For any Product -> Foo other than Aged Brie,Sulphurus,BackStage Quality Decreases by 2 when it"s Sellin date is 0', function() {
+//         const gildedRose = new GildedRose([ new Item('Foo', 0, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(43);
+//     });
+
+//     it('For any item "Foo" which is not sulphurus Sellin value reduces to 1', function() {
+//         const gildedRose = new GildedRose([ new Item('Foo', 15, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].sellIn).to.equal(14);
+//     });
+//     it('For any item "Backstage" which is not sulphurus Sellin value reduces to 1 ', function() {
+//         const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 6, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].sellIn).to.equal(5);
+//     });
+//     it('For any item "Aged Bria" which is not sulphurus Sellin value reduces to 1 ', function() {
+//         const gildedRose = new GildedRose([ new Item('Aged Brie', 6, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].sellIn).to.equal(5);
+//     });
+//     it('For Sulphurus Sellin value Should be same ', function() {
+//         const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 6, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].sellIn).to.equal(6);
+//     });
+
+//     it('Conjured Mana Cake degrade twice as fast as a normal item.', function() {
+//         const gildedRose = new GildedRose([ new Item('Conjured Mana Cake', 6, 45) ]);
+//         const items = gildedRose.updateQuality();
+//         expect(items[0].quality).to.equal(43);
+//     });  
+// });
+describe('Golden Master Test', function () {
+
+    describe('Backstage passes to a TAFKAL80ETC concert tests', function () {
+
+        const backStagePassesName = "Backstage passes to a TAFKAL80ETC concert";
+
+        it('quality less than 50, quality should increase by 1', function () {
+            const gildedRose = new GildedRose([ new Item(backStagePassesName, 12, 0) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].name).to.equal(backStagePassesName);
+            expect(items[0].quality).to.equal(1);
+        });
+
+        it('quality less than 50, 5 < sellIn < 11, quality should increase by 2', function () {
+            const gildedRose = new GildedRose([ new Item(backStagePassesName, 6, 0) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].name).to.equal(backStagePassesName);
+            expect(items[0].quality).to.equal(2);
+        });
+
+        it('quality less than 50, sellIn < 6, quality should increase by 3', function () {
+            const gildedRose = new GildedRose([ new Item(backStagePassesName, 5, 0) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].name).to.equal(backStagePassesName);
+            expect(items[0].quality).to.equal(3);
+        });
+
+        it('quality is 49, sellIn < 6, quality should increase by 1', function () {
+            const gildedRose = new GildedRose([ new Item(backStagePassesName, 5, 49) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].name).to.equal(backStagePassesName);
+            expect(items[0].quality).to.equal(50);
+        });
+
+        it('quality less than 50, sellIn < 1, sellIn should be -1, quality should be 0', function () {
+            const gildedRose = new GildedRose([ new Item(backStagePassesName, 0, 0) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].name).to.equal(backStagePassesName);
+            expect(items[0].sellIn).to.equal(-1);
+            expect(items[0].quality).to.equal(0);
+        });
+    });
+
+    describe('Aged Brie tests', function () {
+
+        const agedBrieName = "Aged Brie";
+
+        it('quality less than 50, quality should increase by 1', function () {
+            const gildedRose = new GildedRose([ new Item(agedBrieName, 12, 0) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].name).to.equal(agedBrieName);
+            expect(items[0].quality).to.equal(1);
+        });
+
+        it('quality > 50, quality should not change', function () {
+            const gildedRose = new GildedRose([ new Item(agedBrieName, 12, 50) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].name).to.equal(agedBrieName);
+            expect(items[0].quality).to.equal(50);
+        });
+
+        it('quality less than 50, sellIn < 1, quality should increase by 2', function () {
+            const gildedRose = new GildedRose([ new Item(agedBrieName, 0, 0) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].name).to.equal(agedBrieName);
+            expect(items[0].quality).to.equal(2);
+        });
+
+        it('quality > 50, sellIn < 1, quality should not change', function () {
+            const gildedRose = new GildedRose([ new Item(agedBrieName, 0, 50) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].name).to.equal(agedBrieName);
+            expect(items[0].quality).to.equal(50);
+        });
+    });
+
+    describe('Sulfuras, Hand of Ragnaros tests', function () {
+
+        const handOfRagnarosName = "Sulfuras, Hand of Ragnaros";
+
+        it('does not change', function () {
+            const gildedRose = new GildedRose([ new Item(handOfRagnarosName, 12, 5) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].name).to.equal(handOfRagnarosName);
+            expect(items[0].quality).to.equal(5);
+            expect(items[0].sellIn).to.equal(12);
+        });
+
+        it('does not change, even with low values', function () {
+            const gildedRose = new GildedRose([ new Item(handOfRagnarosName, -1, 5) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].name).to.equal(handOfRagnarosName);
+            expect(items[0].quality).to.equal(5);
+            expect(items[0].sellIn).to.equal(-1);
+        });
+    });
+
+    describe('Others tests', function () {
+
+        const otherName = "Other";
+
+        it('0 < quality < 50, quality should decrease by 1', function () {
+            const gildedRose = new GildedRose([ new Item(otherName, 12, 2) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].name).to.equal(otherName);
+            expect(items[0].quality).to.equal(1);
+            expect(items[0].sellIn).to.equal(11);
+        });
+
+        it('sellIn < 0, quality > 0, should decrease by 1', function () {
+            const gildedRose = new GildedRose([ new Item(otherName, -1, 2) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].name).to.equal(otherName);
+            expect(items[0].quality).to.equal(0);
+            expect(items[0].sellIn).to.equal(-2);
+        });
+
+        it('sellIn < 0, quality < 0, should not change', function () {
+            const gildedRose = new GildedRose([ new Item(otherName, -1, -2) ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].name).to.equal(otherName);
+            expect(items[0].quality).to.equal(-2);
+            expect(items[0].sellIn).to.equal(-2);
+        });
+    });
+
+
+    describe('My tests',function(){
+            it('Aged Brie Quality increases by 1 as its Sellin value approaches when there are 5 days ', function() {
         const gildedRose = new GildedRose([ new Item('Aged Brie', 5, 45) ]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).to.equal(46);
     });
-
-    it('Aged Brie Quality increases by 1 as its Sellin value approaches less than 11 ', function() {
+    
+    it('Aged Brie Quality increases by 1 as its Sellin value approaches when there are 10 days', function() {
         const gildedRose = new GildedRose([ new Item('Aged Brie', 10, 45) ]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).to.equal(46);
     });
-    it('Aged Brie Quality increases by 1 as its Sellin value greater than 15 ', function() {
+    it('Aged Brie Quality increases by 1 as its Sellin value apporaches ', function() {
         const gildedRose = new GildedRose([ new Item('Aged Brie', 15, 45) ]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).to.equal(46);
@@ -42,6 +294,17 @@ describe('Gilded Rose', function () {
         const items = gildedRose.updateQuality();
         expect(items[0].quality).to.equal(47);
     });
+    it('Backstage passes Quality increases by 1 when there are 11 days', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 11, 45) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(46);
+    });
+
+    it('Backstage passes Quality increases by 2 when there are 6 days', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 6, 45) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(47);
+    });
 
     it('Backstage passes Quality increases by 3 when there are 5 days', function() {
         const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 5, 45) ]);
@@ -60,18 +323,18 @@ describe('Gilded Rose', function () {
         expect(items[0].quality).to.equal(0);
     });
 
-    it('For any Product -> Foo other than Aged Brie,Sulphurus,BackStage Quality Decreases by 1 as its Sellin value approaches less than 6 ', function() {
+    it('For any Product -> Foo other than Aged Brie,Sulphurus,BackStage Quality Decreases by 1 when there are 5 days ', function() {
         const gildedRose = new GildedRose([ new Item('Foo', 5, 45) ]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).to.equal(44);
     });
 
-    it('For any Product -> Foo other than Aged Brie,Sulphurus,BackStage Quality Decreases by 1 as its Sellin value approaches less than 11 ', function() {
+    it('For any Product -> Foo other than Aged Brie,Sulphurus,BackStage Quality Decreases by 1 when there are 10 days ', function() {
         const gildedRose = new GildedRose([ new Item('Foo', 10, 45) ]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).to.equal(44);
     });
-    it('For any Product -> Foo other than Aged Brie,Sulphurus,BackStage Quality Decreases by 1 as its Sellin value approaches greater than 11 ', function() {
+    it('For any Product -> Foo other than Aged Brie,Sulphurus,BackStage Quality Decreases by 1 as its Sellin value approaches', function() {
         const gildedRose = new GildedRose([ new Item('Foo', 15, 45) ]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).to.equal(44);
@@ -114,6 +377,7 @@ describe('Gilded Rose', function () {
         const gildedRose = new GildedRose([ new Item('Conjured Mana Cake', 6, 45) ]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).to.equal(43);
+    });
     });
 
 });
